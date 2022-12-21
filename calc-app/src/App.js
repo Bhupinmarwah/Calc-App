cc0f525bac0f34d7747f14d7ff91c2fed3e872f8
Bhupin Marwah <Bhupinm@gmail.com>
2022-12-21 13:56:55 -0500

    Changes
    Changed font size and added css styling for paragraph.

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -46,7 +46,7 @@ function App() {
         <h1>Simplest Working Calculator</h1> 
       </div> 
       <form> 
-        <p ref={resultRef}> {result} </p> 
+        <p ref={resultRef}> Result:   {result} </p> 
         <input
           pattern="[0-9]" 
           ref={inputRef} 
